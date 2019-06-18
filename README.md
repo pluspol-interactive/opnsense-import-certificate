1fd73a88cbc0ea7e20acc362e2ab7e4a6b8b92df
Falko Trojahn <trojahn@pluspol.info>
2019-06-18 14:50:58 +0200

    README: explain usage PHP: fix differences for usage with OPNsense instead of pfsense

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# pfsense-import-certificate
-Script to import an SSL certificate into a running pfsense system
+# opnsense-import-certificate
+Script to import an SSL certificate into a running opnsense system
+
+## usage
+
+
+```
+Usage: php opnsense-import-certificate.php /path/to/certificate.crt /path/to/private/key.pem cert_hostname.domain.tld
+```
+
+## automation example with dehydrated acme
+
+```
+./dehydrated -c -f config-lan
+# push cert to opnsense
+scp opnsense-import-certificate.php certs/yourhost.domain.tld/privkey.pem certs/yourhost.domain.tld/fullchain.pem root@youropnsensehost: \
+&& ssh root@youropnsensehost 'php opnsense-import-certificate.php /root/fullchain.pem /root/privkey.pem yourhost.domain.tld'
+```
--- opnsense-import-certificate.php
+++ opnsense-import-certificate.php
@@ -10,19 +10,19 @@ if (empty($argc)) {
 	die(1);
 }
 
-if ($argc != 3) {
-	echo "Usage: php " . $argv[0] . " /path/to/certificate.crt /path/to/private/key.pem\r\n";
+if ($argc != 4) {
+	echo "Usage: php " . $argv[0] . " /path/to/certificate.crt /path/to/private/key.pem cert_hostname.domain.tld\r\n";
 	die(1);
 }
 
+require_once "config.inc";
 require_once "certs.inc";
-require_once "pfsense-utils.inc";
-require_once "functions.inc";
+require_once "util.inc";
 require_once "filter.inc";
-require_once "shaper.inc";
 
 $certificate = trim(file_get_contents($argv[1]));
 $key = trim(file_get_contents($argv[2]));
+$hostname = trim($argv[3]);
 
 // Do some quick verification of the certificate, similar to what the GUI does
 if (empty($certificate)) {
@@ -30,23 +30,27 @@ if (empty($certificate)) {
 	die(1);
 }
 if (!strstr($certificate, "BEGIN CERTIFICATE") || !strstr($certificate, "END CERTIFICATE")) {
-	echo "This certificate does not appear to be valid.\r\n";
+	echo "This certificate does not appear to be valid.\r\nOr: cert and privkey args switched?\r\n";
 	die(1);
 }
 
-// Verification that the certificate matches the key
+// Verification that the certificate matches
 if (empty($key)) {
 	echo "The key is empty.\r\n";
 	die(1);
 }
-if (cert_get_publickey($certificate, false) != cert_get_publickey($key, false, 'prv')) {
-	echo "The private key does not match the certificate.\r\n";
+if (trim(cert_get_subject($certificate, false)) != "CN=".$hostname.",") {
+	echo "The certificate subject does not match the hostname $hostname.\r\n".cert_get_subject($certificate, false)."\r\n";
+	die(1);
+}
+if (trim(cert_get_issuer($certificate, false)) != "O=Let's Encrypt, CN=Let's Encrypt Authority X3, C=US,") {
+	echo "The certificate issuer does not match the certificate.\r\n".cert_get_issuer($certificate, false)."\r\n";
 	die(1);
 }
 
 $cert = array();
 $cert['refid'] = uniqid();
-$cert['descr'] = "Certificate added to pfsense through " . $argv[0] . " on " . date("Y/m/d");
+$cert['descr'] = "Certificate added to opnsense through " . $argv[0] . " on " . date("Y/m/d");
 
 cert_import($cert, $certificate, $key);
 
@@ -90,7 +94,7 @@ $config['system']['webgui']['ssl-certref'] = $cert['refid'];
 
 write_config();
 
-log_error(gettext("webConfigurator configuration has changed. Restarting webConfigurator."));
-send_event("service restart webgui");
+log_error('Web GUI configuration has changed. Restarting now.');
+configd_run('webgui restart 2', true);
 
 echo "Completed! New certificate installed.\r\n";
